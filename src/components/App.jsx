52e86644f6440eccced9d2566907a30bdedac0af
blavikensbutcher <v.pidnebesnyi@omisoft.net>
2025-12-06 19:20:12 +0200

    bugfix/updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,16 +16,37 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('review', JSON.stringify(stats));
-  }, [stats, setStats]);
+  }, [stats]);
 
-  let totalFeedback = stats.good + stats.neutral + stats.bad;
+  const totalFeedback = stats.good + stats.neutral + stats.bad;
+  
+  const updateFeedback = (type) => {
+    setStats(prevStats => ({
+      ...prevStats,
+      [type]: prevStats[type] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setStats({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const positiveFeedbackPercentage = totalFeedback > 0 ? Math.round((stats.good / totalFeedback) * 100) : 0;
 
   return (
     <>
       <Description />
-      <Options stats={stats} setStats={setStats} total={totalFeedback} />
+      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} total={totalFeedback} />
 
-      {totalFeedback > 0 ? <Feedback stats={stats} total={totalFeedback} /> : <Notification />}
+      {totalFeedback > 0 ? (
+        <Feedback stats={stats} total={totalFeedback} positiveFeedbackPercentage={positiveFeedbackPercentage} />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 }
